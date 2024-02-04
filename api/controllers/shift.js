7f2af8d84b849012d13af9b60551e646ce023337
Emerson Jordan <emerz210@gmail.com>
2024-02-03 20:43:02 -0800

    add add shift

--- api/controllers/shift.js
+++ api/controllers/shift.js
@@ -24,7 +24,26 @@ export const getShift = (req,res)=>{
 }
 
 export const addShift = (req,res)=>{
-    res.json("from controller shifts");
+    const token = req.cookies.access_token;
+    if (!token) return res.status(401).json ("Not authenticated");
+
+    jwt.verify(token, "jwtkey", (err, userInfo) => {
+        if (err) return res.status(403).json("Token is not valid!");
+
+        const q =
+        "INSERT INTO shifts(`starttime`, `endtime`, `uid`) VALUES (?)";
+
+        const values = [
+            req.body.starttime,
+            req.body.endtime,
+            req.body.uid
+        ]
+
+        db.query(q, [values], (err, data) =>{
+            if (err) return res.status(500).json(err);
+            return res.json("shift has been created");
+        })
+    })
 }
 
 export const deleteShift = (req,res)=>{
